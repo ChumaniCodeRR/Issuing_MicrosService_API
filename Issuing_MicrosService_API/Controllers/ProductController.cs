d19a1cc5080c3233876950c9d10a38440ba829ae
Chumani <chumani@vetro.co.za>
2021-04-08 14:03:29 +0200

    First commit

---
+++ Issuing_MicrosService_API/Controllers/ProductController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Issuing_MicrosService_API.Model;
+using System.Transactions;
+using Issuing_MicrosService_API.Repository;
+using Microsoft.AspNetCore.DataProtection;
+
+namespace Issuing_MicrosService_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly IDataProtector _protector;
+
+        // GET: ProductController
+
+        public ProductController(IProductRepository productRepository, IDataProtectionProvider provider)
+        {
+            _productRepository = productRepository;
+            _protector = provider.CreateProtector(GetType().FullName);
+        }
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            Product p = new Product();
+
+            var products = _productRepository.GetProducts();
+            var protectedData = _protector.Protect("Protected Data");
+            p.Name = protectedData;
+            p.Description = protectedData;
+            p.Price = Convert.ToDecimal(protectedData);
+            p.CategoryId = Convert.ToInt32(protectedData);
+
+            return new OkObjectResult(products);
+        }
+
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var product = _productRepository.GetProductByID(id);
+            return new OkObjectResult(product);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            using (var scope = new TransactionScope())
+            {
+                _productRepository.InsertProduct(product);
+                scope.Complete();
+                return CreatedAtAction(nameof(Get), new { id = product.Id }, product);
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Product product)
+        {
+            if (product != null)
+            {
+                using (var scope = new TransactionScope())
+                {
+                    _productRepository.UpdateProduct(product);
+                    scope.Complete();
+                    return new OkResult();
+                }
+            }
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _productRepository.DeleteProduct(id);
+            return new OkResult();
+        }
+    }
+}
---
+++ Issuing_MicrosService_API/Repository/ProductRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Issuing_MicrosService_API.Model;
+using Microsoft.EntityFrameworkCore;
+using Issuing_MicrosService_API.DBContexts;
+
+namespace Issuing_MicrosService_API.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ProductContext _dbContext;
+
+        public ProductRepository(ProductContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteProduct(int productId)
+        {
+            var product = _dbContext.Products.Find(productId);
+            _dbContext.Products.Remove(product);
+            Save();
+        }
+
+        public Product GetProductByID(int productId)
+        {
+            return _dbContext.Products.Find(productId);
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return _dbContext.Products.ToList();
+        }
+
+        public void InsertProduct(Product product)
+        {
+            _dbContext.Add(product);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _dbContext.Entry(product).State = EntityState.Modified;
+            Save();
+        }
+
+        //public void DeleteProduct(int ProductId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Product GetProductByID(int ProductId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public IEnumerable<Product> GetProducts()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void Save()
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public void InsertProduct(Product Product)
+        //{
+        //    throw new NotImplementedException();
+        //}
+        //public void UpdateProduct(Product Product)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
